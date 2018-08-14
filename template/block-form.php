d606f1a9fee42c71b6e4ec43c2052a0a1e9b1bca
Josh Armentano <josh@abidewebdesign.com>
2018-08-14 09:51:35 -0700

    Updates to form page block

--- template/block-form.php
+++ template/block-form.php
@@ -1,9 +1,14 @@
-<?php $form_object = get_sub_field('your_form_field'); ?>
+<?php $form_object = get_sub_field('form'); ?>
 <!-- Form Content Block -->
 <div class="basic-content-block">
 	<div class="row justify-content-center">
 		<div class="col-lg-8">
-			<h2 class="mb-4"><?php $form_object['title']; ?></h2>
+			<?php if(get_sub_field('form_title')): ?>
+				<h2 class="mb-3"><?php the_sub_field('form_title'); ?></h2>
+			<?php endif; ?>
+			<?php if(get_sub_field('form_description')): ?>
+				<p class="mb-3"><?php the_sub_field('form_description') ?></p>
+			<?php endif; ?>
 			<?php echo do_shortcode('[gravityform id="' . $form_object['id'] . '" title="false" description="false" ajax="true"]'); ?>
 		</div>
 	</div>
