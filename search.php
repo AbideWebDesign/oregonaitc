ec9bf23aad327a1e2bb56c655f914333da0319fe
Josh Armentano <josh@abidewebdesign.com>
2018-10-02 09:40:12 -0700

    Update: Search results and styling..

--- search.php
+++ search.php
@@ -19,7 +19,25 @@
 						<div class="card-body">
 							<h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
 							<p class="text-sm text-gray mt-1 mb-3 pb-2 	border-1 border-bottom"><?php echo get_the_permalink(); ?></p>
-							<div class="text-md text-description"><?php the_excerpt(); ?></div>
+							<div class="text-md text-description">
+								<div class="row">
+									<?php if(get_field('resource_image')): ?>
+										<?php $image = get_field('resource_image'); ?>
+										<div class="col-3 col-lg-2">
+											<a href="<?php the_permalink(); ?>"><?php echo wp_get_attachment_image($image['id'], 'thumbnail', false, array('class'=>'img-fluid')); ?></a>
+										</div>
+										<div class="col-9 col-lg-10">
+											<strong>Resource Description</strong>
+											<div class="text-description mb-3"><?php the_field('resource_description'); ?></div>
+											<p class="m-0 text-md" id="author">By: <?php the_field('resource_authors'); ?></p>
+										</div>
+									<?php else: ?>
+										<div class="col-12">
+											<?php the_excerpt(); ?>
+										</div>
+									<?php endif; ?>
+								</div>
+							</div>
 						</div>
 					</div>
 				<?php endwhile; ?>
