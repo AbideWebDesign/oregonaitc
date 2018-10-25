881733eeb47bf55be3d655684a4dd3140117ade7
Josh Armentano <josh@abidewebdesign.com>
2018-10-25 10:16:51 -0700

    Added: Lost Username template.

---
+++ page-members-username.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Members Username Lost */ ?>
+<?php get_header(); ?>
+<div class="section section-alt section-sm">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-4">
+				<div class="card">
+					<div class="card-body">
+						<?php echo do_shortcode('[wpmem_form forgot_username]'); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
+<?php get_footer(); ?>	
