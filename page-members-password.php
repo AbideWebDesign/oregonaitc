a69cc2e00295d07a5c4d39b6ab173c614453b449
Josh Armentano <josh@abidewebdesign.com>
2018-10-25 09:19:46 -0700

    Added: Password Reset template.

---
+++ page-members-password.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Members Password Reset */ ?>
+<?php get_header(); ?>
+<?php if (have_posts()) : ?>
+	<?php while (have_posts()) : the_post(); ?>
+	<div class="section section-alt section-sm">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-4">
+					<div class="card">
+						<div id="password-reset" class="card-body">
+							<?php echo do_shortcode('[wpmem_form password]'); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php endwhile; ?>
+<?php endif; ?>	
+<?php get_footer(); ?>	
