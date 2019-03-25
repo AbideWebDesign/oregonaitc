4be9e89f0a93a315d4b9736ed9e3e2b0f95f7f58
Josh Armentano <josh@abidewebdesign.com>
2019-03-25 08:55:33 -0700

    Added: URL parameter handling for matrix library items.

--- page-matrix.php
+++ page-matrix.php
@@ -1,9 +1,16 @@
 <?php /* Template Name: Matrix */ ?>
 <?php get_header(); ?>
+<?php 
+	if (isset($_GET['lpid'])) {
+		$url = "https://agclassroom.org/oregon/matrix/lessonplan.cfm?lpid=" . $_GET['lpid'];
+	} else {
+		$url = "https://agclassroom.org/oregon/matrix/";
+	}
+?>
 <div class="section section-sm section-alt">
 	<div class="container">
 		<div class="embed-responsive embed-responsive-4by3 bg-white">
-			<iframe class="embed-responsive-item" src="https://agclassroom.org/oregon/matrix/"></iframe>
+			<iframe class="embed-responsive-item" src="<?php echo $url; ?>"></iframe>
 		</div>
 	</div>
 </div>
